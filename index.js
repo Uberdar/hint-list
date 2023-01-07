55c4197510c20468ad1da7dd49f8a86ce4c548d7
Alexandr Sanatar <allexvik@gmail.com>
2023-01-07 21:35:18 +0200

    upd

---
+++ index.js
@@ -0,0 +1,53 @@
+// const x = [1, 2, 3];
+// console.log(x);
+// x.hello = ":)";
+// console.log("x: ", x);
+// console.log(x.hello);
+
+// const xfn = function () {
+//   //   console.log("this is a function");
+// };
+// console.log(xfn());
+// console.dir(xfn);
+// xfn.hello = ":)";
+// console.log(xfn.hello);
+
+// objects
+// const username = "testname";
+// const usermail = "testmail";
+// const testobj = {
+//   username,
+//   usermail,
+// };
+// console.log("testobj: ", testobj);
+
+// const inputname = "color";
+// const inputcolor = "red";
+// const object = {
+//   [inputname]: inputcolor,
+// };
+// console.log("object: ", object);
+
+// const playlist = {
+//   name: "my play list",
+//   rating: 5,
+//   tracks: ["track-1", "track-2", "track-3"],
+//   //   getname: function () {
+//   //     console.log("this is getname function inside playlist obj");
+//   //   },
+//   // oldschool
+//   getname() {
+//     console.log("this is obj method");
+//     console.log(`gets name:`, this.name);
+//   },
+// };
+// // console.log("playlist: ", playlist);
+// // playlist.getname();
+// const {
+//   name: myTracklistName = "defautl name",
+//   rating = 5,
+//   tracks: numberOfMyTracks,
+// } = playlist;
+// console.log("myTracklistName: ", myTracklistName);
+// console.log("rating: ", rating);
+// console.log("numberOfMyTracks: ", numberOfMyTracks);
